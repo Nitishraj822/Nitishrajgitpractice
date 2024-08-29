64a02cbaa33c9681ef2eddd0896818a85954348a
Nitishraj822 <89899009+Nitishraj822@users.noreply.github.com>
2024-08-29 18:20:01 +0530

    test

--- Calculator.py
+++ Calculator.py
@@ -1,11 +1,11 @@
 class Calculator():
+    dfjjjfjoiejw
     def __init__(self,a,b):
         self.a=a
         self.b=b
         
     def add(self):
         return self.a + self.b
-    
-rndehb
+
 a=Calculator(34,35)
 print(a.add())
